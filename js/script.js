f8bbaf9f5e51fab8ef91140f64aea1edaa42b2e0
lorenzo-de-giorgi <lorenzodegiorgi2004@gmail.com>
2024-03-11 16:30:52 +0100

    exercise completed with dice game

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+// random number
+function getRndInteger(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+// email check
 let checkButton = document.getElementById('btn');
 checkButton.addEventListener('click', function(){
         let email = ['lorenzo@gmail.com', 'samu@gmail.com', 'gianmarco@gmail.com', 'nicola@gmail.com'];
@@ -13,5 +18,30 @@ checkButton.addEventListener('click', function(){
                 result.innerHTML = 'Utente Trovato'
         } else {
                 result.innerHTML = 'Utente NON Trovato'
+        }
+})
+// gioco dadi
+let dadiGame = document.getElementById('game-d');
+dadiGame.addEventListener('click', function(){
+        let number = document.getElementById('number');
+        let winner = document.getElementById('winner')
+        let randomNumberUser = getRndInteger(1, 6);
+        let randomNumberComputer = getRndInteger(1, 6);
+        if(randomNumberUser > randomNumberComputer){
+                number.innerHTML = `
+                <p>Il computer aveva "${randomNumberComputer}". Hai vinto con il numero:</p>
+                `;
+                winner.innerHTML = `
+                <p><img src="img/${randomNumberUser}.svg" alt=""></p>
+                `;       
+        } else if(randomNumberUser == randomNumberComputer){
+                number.ht
+        } else {
+                number.innerHTML = `
+                <p>Avevi il numero: "${randomNumberUser}". Il computer ha vinto con il numero:</p>
+                `;
+                winner.innerHTML = `
+                <p><img src="img/${randomNumberComputer}.svg" alt=""></p>
+                `;
         }
 })
